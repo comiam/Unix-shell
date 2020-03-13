4525b00e3b5a73bc345113c90bae71ff52668a64
Maxim <maxim.bolshim@yandex.ru>
2020-03-13 10:18:08 +0700

    Bug fixes and pretty updates)

--- shell.c
+++ shell.c
@@ -1,14 +1,19 @@
-#include <sys/types.h>
+#define _XOPEN_SOURCE 500
 #include <stdlib.h>
 #include <termio.h>
+#include <signal.h>
 #include "shell.h"
+
+void closeZombies();
+int getInvite();
 
 char *infile, *outfile, *appfile;
 struct command cmds[MAXCMDS];
 char bkgrnd;
 char hostname[HOST_NAME_MAX];
 char username[LOGIN_NAME_MAX];
-struct termios __termios_p;
+char prompt[HOST_NAME_MAX + LOGIN_NAME_MAX + MAX_DIRECTORY_SIZE + 5];
+struct termios term_strct;
 
 int main(int argc, char *argv[])
 {
@@ -17,39 +22,37 @@ int main(int argc, char *argv[])
     char varline[4096];
     char dir[MAX_DIRECTORY_SIZE];
     int ncmds;
-    char prompt[HOST_NAME_MAX + LOGIN_NAME_MAX + MAX_DIRECTORY_SIZE + 5];
     int inner_cmd_stat = 0;
 
-    tcgetattr(0, &__termios_p);
+    /* SAVE TERMINAL ATTRIBUTES */
+    tcgetattr(0, &term_strct);
 
-    /* PLACE SIGNAL CODE HERE */
-    signal(SIGINT, SIG_IGN);
+    /* PLACE sigset CODE HERE */
+    sigset(SIGINT, SIG_IGN);
+    sigset(SIGCHLD, closeZombies);
 
+    /* INIT HOME LOCATION IN SHELL */
     init_home(argv[0]);
+
+    /* INIT NAME AND HOSTNAME */
     gethostname(hostname, HOST_NAME_MAX);
     getlogin_r(username, LOGIN_NAME_MAX);
 
     get_dir_prompt(dir);
     sprintf(prompt, "%s@%s:%s$ ", username, hostname, dir);
 
-    write(1, prompt, strlen(prompt));
-    char start = 1;
-    while (prompt_line(line, sizeof(line)) > 0)
+    while (getInvite() && prompt_line(line, sizeof(line)) > 0)
     {
-        if(!start)
-            write(1, prompt, strlen(prompt));
-        else
-            start = 0;
-
         if ((ncmds = parse_line(line, varline)) <= 0)
             continue;
 
         #ifdef DEBUG
         {
-           int i, j;     for (i = 0; i < ncmds; i++) {
-               for (j = 0; cmds[i].cmdargs[j] != (char *) NULL; j++)
-                   fprintf(stderr, "cmd[%d].cmdargs[%d] = %s\n",
-                    i, j, cmds[i].cmdargs[j]);
+            int i, j;
+            for (i = 0; i < ncmds; i++)
+            {
+                for (j = 0; cmds[i].cmdargs[j] != (char *) NULL; j++)
+                    fprintf(stderr, "cmd[%d].cmdargs[%d] = %s\n", i, j, cmds[i].cmdargs[j]);
                 fprintf(stderr, "cmds[%d].cmdflag = %o\n", i, cmds[i].cmdflag);
             }
         }
@@ -72,9 +75,9 @@ int main(int argc, char *argv[])
             {
                 if(bkgrnd)
                 {
-                    signal(SIGINT, SIG_IGN);
-                    signal(SIGQUIT, SIG_IGN);
-                    signal(SIGHUP, SIG_IGN);
+                    sigset(SIGINT, SIG_IGN);
+                    sigset(SIGQUIT, SIG_IGN);
+                    sigset(SIGHUP, SIG_IGN);
                 }
                 if(!i && infile)
                 {
@@ -83,7 +86,7 @@ int main(int argc, char *argv[])
                     {
                         if(dup2(input, STDIN_FILENO) == -1)
                         {
-                            perror("Couldn't redirect input");
+                            perror("Couldn't redirect input\n");
                             close(input);
                             return EXIT_FAILURE;
                         }
@@ -91,7 +94,7 @@ int main(int argc, char *argv[])
                     }
                     else
                     {
-                        perror("Couldn't open input file");
+                        perror("Couldn't open input file\n");
                         return EXIT_FAILURE;
                     }
                 }
@@ -107,7 +110,7 @@ int main(int argc, char *argv[])
                     {
                         if(dup2(output, STDOUT_FILENO) == -1)// SET STDOUT_FILENO = OUTPUT
                          {
-                            perror("Couldn't redirect output");
+                            perror("Couldn't redirect output\n");
                             close(output);
                             return EXIT_FAILURE;
                         }
@@ -115,7 +118,7 @@ int main(int argc, char *argv[])
                     }
                     else
                     {
-                        perror("Couldn't open output file");
+                        perror("Couldn't open output file\n");
                         return EXIT_FAILURE;
                     }
                 }
@@ -135,20 +138,33 @@ int main(int argc, char *argv[])
                     if(!WIFEXITED(status))
                     {
                         if(WIFSIGNALED(status))
-                            fprintf(stderr, "Process %u stopped due to an unprocessed signal %u\n", pid, WTERMSIG(status));
+                            fprintf(stderr, "Process %u stopped due to an unprocessed sigset %u\n", pid, WTERMSIG(status));
                         else
-                            fprintf(stderr, "The process %u failed with a signal %u\n", pid, WEXITSTATUS(status));
+                            fprintf(stderr, "The process %u failed with a sigset %u\n", pid, WEXITSTATUS(status));
 
-                        tcsetattr(0, TCSANOW, &__termios_p);
+                        tcsetattr(0, TCSANOW, &term_strct);
                     }
                 }
                 else
                     printf("Background pid: %ld\n", (long)pid);
             }
             else
-                perror("Couldn't create process");
+                perror("Couldn't create process\n");
         }
     }
     return EXIT_SUCCESS;
 }
-/* PLACE SIGNAL CODE HERE */
+
+int getInvite()
+{
+    write(STDOUT_FILENO, prompt, strlen(prompt));
+    fflush(stdout);
+
+    return 1;
+}
+
+/* PLACE sigset CODE HERE */
+void closeZombies()
+{
+    waitpid(-1, NULL, WNOHANG);
+}
